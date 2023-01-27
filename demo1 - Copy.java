7efa5a4aaba1eb8b0b91086139d15c5f111cac0a
sahiljaiswal-pw <sahilcourses2@gmail.com>
2023-01-27 11:17:20 +0530

    remove noe method

--- demo1 - Copy.java
+++ demo1 - Copy.java
@@ -11,8 +11,3 @@ public static void main(String args[]){
 system.out.println("once again hello");
 }
 }
-public class sahil{
-public static void main(String args[]){
-system.out.println("once again hello");
-}
-}
