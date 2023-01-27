b880a15577845147d5147cfa5f919c2e8700b68e
sahiljaiswal-pw <sahilcourses2@gmail.com>
2023-01-27 11:08:32 +0530

    added 2 method

--- demo1 - Copy.java
+++ demo1 - Copy.java
@@ -6,3 +6,13 @@ public static void main(String args[]){
 system.out.println("once again hello");
 }
 }
+public class sahil{
+public static void main(String args[]){
+system.out.println("once again hello");
+}
+}
+public class sahil{
+public static void main(String args[]){
+system.out.println("once again hello");
+}
+}
